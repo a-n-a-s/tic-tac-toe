a92c070062c75f041ca7d54657b4b75d25ac1c0b
a-n-a-s <mirzaanas815@gmail.com>
2021-08-26 20:55:36 +0530

    favicon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Board from "./Components/Board";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./index.css";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Board />
+      </div>
+    );
+  }
+}
+
+// Render the App component into DOM
+ReactDOM.render(<App />, document.getElementById("root"));
